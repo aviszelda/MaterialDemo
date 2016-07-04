80ba85d6120db203ea68fddd07b806fe707f902d
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-07-04 10:47:49 -0600

    new submenu
    new submenu

--- app/src/main/java/com/mobtion/materialdemo/com/mobtion/materialdemo/search/SearchFragment.java
+++ app/src/main/java/com/mobtion/materialdemo/com/mobtion/materialdemo/search/SearchFragment.java
@@ -1,20 +1,41 @@
 package com.mobtion.materialdemo.com.mobtion.materialdemo.search;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
 import com.mobtion.materialdemo.MainAbsFragment;
+import com.mobtion.materialdemo.MainActivity;
 import com.mobtion.materialdemo.R;
+import com.mobtion.materialdemo.com.mobtion.materialdemo.adapters.DividerItemDecoration;
+import com.mobtion.materialdemo.com.mobtion.materialdemo.adapters.listAdapter;
+import com.mobtion.materialdemo.com.mobtion.materialdemo.resources.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends MainAbsFragment {
 
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    listAdapter mAdapter;
+    private static final int VERTICAL_ITEM_SPACE = 10;
+
+    List<Person> persons;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -25,7 +46,107 @@ public class SearchFragment extends MainAbsFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.search_fragment, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        //mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+
+        Person.initializeData();
+
+        persons = Person.persons;
+
+        ArrayList<Person> lista = (ArrayList<Person>) persons;
+
+        mAdapter = new listAdapter(lista);
+
+//        mAdapter.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                System.out.println("Person: "+mRecyclerView.getChildAdapterPosition(v));
+//
+//                //Menu menu = MainActivity.navigationView.getMenu();
+//                //MenuItem nav_camara = MainActivity.menu.findItem(R.id.nav_gallery);
+//                //nav_camara.setTitle("NewTitleForCamera");
+//
+//               // ((MainActivity) getActivity()).addFragmentToStack(new detailFragment());
+//            }
+//        });
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        //mRecyclerView.addItemDecoration(new DividerItemDecoration());
+        //mRecyclerView.addItemDecoration(new VerticalSpaceItemDecoration(VERTICAL_ITEM_SPACE));
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), mRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Person person = persons.get(position);
+                Toast.makeText(getActivity(), person.getName() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
         return view;
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
     }
 
 }
