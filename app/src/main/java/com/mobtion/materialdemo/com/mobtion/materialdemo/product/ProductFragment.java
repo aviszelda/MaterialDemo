acc57ff9901ffa46c1e4ffebf1687bc4b809cf11
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-07-06 15:39:12 -0600

    new gallery
    new gallery

--- app/src/main/java/com/mobtion/materialdemo/com/mobtion/materialdemo/product/ProductFragment.java
+++ app/src/main/java/com/mobtion/materialdemo/com/mobtion/materialdemo/product/ProductFragment.java
@@ -1,31 +1,76 @@
 package com.mobtion.materialdemo.com.mobtion.materialdemo.product;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.mobtion.materialdemo.MainAbsFragment;
+import com.mobtion.materialdemo.MainAbsFragmentActivity;
 import com.mobtion.materialdemo.R;
+import com.mobtion.materialdemo.com.mobtion.materialdemo.report.ReportFragment;
+import com.mobtion.materialdemo.com.mobtion.materialdemo.resources.Constants;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductFragment extends MainAbsFragment {
 
+    private Button cameraButton;
+    private Bitmap mImageBitmap;
+    private ImageView mImageView;
 
     public ProductFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.product_fragment, container, false);
+
+        cameraButton = (Button) view.findViewById(R.id.cameraButton);
+        mImageView = (ImageView) view.findViewById(R.id.photoImage);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
         return view;
     }
 
+//    private void handleSmallCameraPhoto(Intent intent) {
+//        Bundle extras = intent.getExtras();
+//        mImageBitmap = (Bitmap) extras.get("data");
+//        mImageView.setImageBitmap(mImageBitmap);
+//        mImageView.setVisibility(View.VISIBLE);
+//    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, Constants.REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Constants.REQUEST_IMAGE_CAPTURE && resultCode == Constants.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            mImageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(mImageBitmap);
+            mImageView.setVisibility(View.VISIBLE);
+            ((MainAbsFragmentActivity)getActivity()).startNewFragment(new ReportFragment());
+        }
+    }
 }
